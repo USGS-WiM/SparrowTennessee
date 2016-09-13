a8ba6f5f1ed56f89f5c009884e4d28b188a0bbc8
esmyers <emyers@usgs.gov>
2016-09-13 16:48:54 -0500

    first commit

---
+++ src/scripts/layers.js
@@ -0,0 +1,107 @@
+/**
+ * Created by bdraper on 4/27/2015.
+ */
+var allLayers;
+
+require([
+    "esri/geometry/Extent",
+    "esri/layers/WMSLayerInfo",
+    "esri/layers/FeatureLayer",
+    'dojo/domReady!'
+], function(
+    Extent,
+    WMSLayerInfo,
+    FeatureLayer
+) {
+
+    allLayers = [
+        {
+            "groupHeading": "Nutrient Model",
+            "showGroupHeading": true,
+            "includeInLayerList": true,
+            "layers": {
+                "phosphorus":{
+                    "url" : "http://gis.wim.usgs.gov/arcgis/rest/services/SparrowTennessee/SparrowTennesseeDev/MapServer/",
+                    "visibleLayers": [0],
+                    "options":{
+                        "id": "PhosphorusLayer",
+                        "opacity": 0.75,
+                        "visible": true
+                    },
+                    "wimOptions":{
+                        "type": "layer",
+                        "layerType": "agisDynamic",
+                        "includeInLayerList": true,
+                        "exclusiveGroupName":"Nutrient Model Results",
+                        "includeLegend" : false
+                        
+                    }
+                },
+                "nitrogen": {
+                    "url" : "http://gis.wim.usgs.gov/arcgis/rest/services/SparrowTennessee/SparrowTennesseeDev/MapServer/",
+                    "visibleLayers": [2],
+                    "options":{
+                        "id": "NitrogenLayer",
+                        "opacity": 0.75,
+                        "visible": false
+                    },
+                    "wimOptions": {
+                        "type": "layer",
+                        "layerType": "agisDynamic",
+                        "includeInLayerList": true,
+                        "exclusiveGroupName":"Nutrient Model Results",
+                        "includeLegend" : false
+                    }
+                }
+            }
+        },
+        {
+            "groupHeading": "Auxilliary Layers",
+            "showGroupHeading": true,
+            "includeInLayerList": true,
+            "layers": {
+                "Tennessee Mainstems" : {
+                    "url": "http://gis.wim.usgs.gov/arcgis/rest/services/SparrowTennessee/SparrowTennesseeDev/MapServer/",
+                    "visibleLayers": [4],
+                    "options": {
+                        "id": "mainstems",
+                        "opacity": 0.75,
+                        "visible": false
+                    },
+                    "wimOptions": {
+                        "type": "layer",
+                        "layerType": "agisDynamic",
+                        "includeInLayerList": true,
+                        "zoomScale": 144448,
+                        "hasOpacitySlider": true,
+                        "hasZoomto" : true,
+                        "includeLegend" : true
+                    }
+                },
+                "Tennessee Streams" : {
+                    "url": "http://gis.wim.usgs.gov/arcgis/rest/services/SparrowTennessee/SparrowTennesseeDev/MapServer/",
+                    "visibleLayers": [5],
+                    "options": {
+                        "id": "streams",
+                        "visible": false,
+                        "opacity": 0.6
+                    },
+                    "wimOptions": {
+                        "type": "layer",
+                        "layerType": "agisDynamic",
+                        "includeInLayerList": true,
+                        "hasOpacitySlider": true,
+                        "hasZoomto": true,
+                        "includeLegend" : false
+                    }
+                }
+            }
+        }
+    ]
+
+});
+
+
+
+
+
---
+++ src/scripts/metric-definitions.js
@@ -0,0 +1,42 @@
+
+//HUC10
+var Group3 = [
+
+	{field: "dl1_g3_tot", name: "Group Aggregate Load delivered to donwstream boundary"},
+	{field: "dy1_g3_tot", name: "Group Aggregate Yield delivered to donwstream boundary"},
+	{field: "dl3_g3_tot", name: "Group Aggregate Load delivered to Group’s outlet"},
+	{field: "dy3_g3_tot", name: "Group Aggregate Yield delivered to Group’s outlet"},
+	{field: "al_g3_tot", name: "Accumulated Load at Group’s outlet"},
+	{field: "ay_g3_tot", name: "Accumulated Yield at Group’s outlet"}
+
+]
+
+//HUC8	
+var Group2 = [
+
+	{field: "dl1_g2_tot", name: "Group Aggregate Load delivered to donwstream boundary"},
+	{field: "dy1_g2_tot", name: "Group Aggregate Yield delivered to donwstream boundary"},
+	{field: "dl2_g2_tot", name: "Group Aggregate Load delivered to Group’s outlet"},
+	{field: "dy2_g2_tot", name: "Group Aggregate Yield delivered to Group’s outlet"}
+
+]
+
+//independent watershed
+var Group1 = [
+
+	{field: "dl1_g1_tot", name: "Group Aggregate Load delivered to donwstream boundary"},
+	{field: "dy1_g1_tot", name: "Group Aggregate Yield delivered to donwstream boundary"}
+
+]
+
+//state
+var ST = [
+
+	{field: "dl1_ST_tot", name: "State Aggregate Load delivered to downstream boundary"},
+	{field: "dy1_ST_TOT", name: "State Aggregate Yield delivered to downstream boundary"},
+	{field: "I_ST_tot", name: "State Aggregate Load"},
+	{field: "y_ST_tot", name: "State Aggregate Yield"}
+
+]
+
+
